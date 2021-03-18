e8bd13034850497ca1cd837e50db7f9bea40fde8
ytnguyen <n01287624@humbermail.ca>
2021-03-17 22:02:24 -0400

    Update RGBv1.ino

--- RGB_Strips/RGBv1/RGBv1.ino
+++ RGB_Strips/RGBv1/RGBv1.ino
@@ -1,94 +1,142 @@
-const boolean IS_COMMON_ANODE = false; 
+#define red_led PA8
+#define green_led PA10
+#define blue_led PA9
 
-#define RGB_RED_PIN = PA10;
-#define RGB_GREEN_PIN = PA9;
-#define RGB_BLUE_PIN = PA8;
-const int DELAY_MS = 1000; // delay in ms between changing colors
+#define READ_TIMEOUT 250
 
-void setup() {
-  // Set the RGB pins to output
-  pinMode(RGB_RED_PIN, OUTPUT);
-  pinMode(RGB_GREEN_PIN, OUTPUT);
-  pinMode(RGB_BLUE_PIN, OUTPUT);
+int messageResult = 0;
+String message = "";
 
-  // Turn on Serial so we can verify expected colors via Serial Monitor
+
+int redBright = 0;
+int greenBright = 0;
+int blueBright = 0;
+
+void setup()
+{
+  pinMode(red_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
+  pinMode(blue_led, OUTPUT);
+  
+  analogWrite(red_led, 0);
+  analogWrite(green_led, 0);
+  analogWrite(blue_led, 0);
   Serial.begin(9600);
   Serial2.begin(115200);
 }
 
 void loop() {
+  if (Serial2.available()) {
+    messageResult = readIncomingMessage();
 
-   if (Serial.available() > 0) 
-  {
-      char state = Serial.read();
-      
-      if (state == 'H' || state == 'h')
-      {
-         Serial.print("LED IS ON ");
-         setRgbLedColor(HIGH, HIGH, HIGH);
-         Serial2.print ("RGB ON"); //send result to ESP8266
-         delay(1000);
-      }
+    if (messageResult == -1) {
+      Serial.println("Error reading message: timed out waiting for new character");
+    } else if (messageResult < -1) {
+      Serial.print("Error reading message: an unkown error occured with code ");
+      Serial.println(messageResult);
+    }
 
-    // red
-      if (state == 'R' || state == 'r')
-      {
-      Serial.print("Red: ");
-      setRgbLedColor(HIGH, LOW, LOW);
-      Serial2.print ("Red ON"); //send result to ESP8266
-      delay(DELAY_MS);  
-      }
-  
-      // green
-      if (state == 'G' || state == 'g')
-      {
-      Serial.print("Green: ");
-      setRgbLedColor(LOW, HIGH, LOW);
-      Serial2.print ("Green ON"); //send result to ESP8266
-      delay(DELAY_MS);  
-      }
-  
-      // blue
-      if (state == 'B' || state == 'b')
-      {
-      Serial.print("Blue: ");
-      setRgbLedColor(LOW, LOW, HIGH);
-      Serial2.print ("Blue ON"); //send result to ESP8266
-      delay(DELAY_MS); 
-      }     
-              
-      if (state == 'L' || state == 'l')
-      {
-         Serial.print("LED IS OFF ");
-         setRgbLedColor(LOW, LOW, LOW);
-         Serial2.print ("RGB OFF"); //send result to ESP8266
-      }
-     }
-    if (Serial2.available()>0){
-    Serial.print((char) Serial2.read()); //Forward all received messages from ESP8266 to main serial
-      }  
+    Serial.print("ESP8266: ");
+    Serial.println(message);
+
+    if (messageResult == 1) {
+      Serial.println("Setting LED brightnesses:");
+      write_leds();
+    }
+
+  }
 }
 
-/**
- * setRgbLedColor takes in either HIGH or LOW for red, green, and blue.
 
- */
-void setRgbLedColor(int red, int green, int blue)
+
+void write_leds()
 {
-  // If a common anode LED, invert values
-  if(IS_COMMON_ANODE == true){
-    red = !red;
-    green = !green;
-    blue = !blue;
+  //if (led == 'r'){
+  //  Serial.println("Received Red input");
+    analogWrite(red_led, redBright);
+    Serial.print ("red:");
+    Serial.println(redBright);
+  //  return;
+ // }
+ // if (led == 'g'){
+  //  Serial.println("Green value");
+    analogWrite(green_led, greenBright);
+    Serial.print ("green:");
+    Serial.println(greenBright);
+  //  return;
+  //}
+ // if (led == 'b'){
+   // Serial.println("Blue value");
+    analogWrite(blue_led, blueBright);
+    Serial.print ("blue:");
+    Serial.println(blueBright);
+   // return;
+  //}
+  
+  return;
+}
+
+int readIncomingMessage(void) {
+  unsigned long millisReadTimeout = millis() + READ_TIMEOUT;
+  char currChar = 0;
+  String intString = "";
+  int numCommas = 0;
+  boolean notData = false;
+
+  message = "";
+
+  while (millis() < millisReadTimeout) {
+    if (Serial2.available() > 0) {
+      currChar = Serial2.read();
+
+      if (currChar == '\n') {
+        if (notData) {
+          return 0; // code 0 = finished reading log message
+        }
+        blueBright = intString.toInt();
+        //Serial.print("Set blue brightness variable to ");
+        //Serial.println(blueBright);
+        return 1; // code 1 = finished reading and interpreting data message
+      }
+      else if (!notData) {
+        if (isDigit(currChar)) {
+          intString += currChar;
+        }
+        else if (currChar == ',') {
+          numCommas++;
+          
+          if (numCommas == 1) {
+            redBright = intString.toInt();
+            //Serial.print("Set red brightness variable to ");
+            //Serial.println(redBright);
+          } else if (numCommas == 2) {
+            greenBright = intString.toInt();
+            //Serial.print("Set green brightness variable to ");
+            //Serial.println(greenBright);
+          } else {
+            Serial.println("message is not data: too many commas");
+            notData = true;
+          }
+          
+          intString = "";
+        }
+        else {
+          //Serial.print("message is not data: char '");
+          //Serial.print(currChar);
+          //Serial.print("' is not used in data\n");
+          notData = true;
+        }
+      }
+
+      if (currChar != '\r'){
+      message += currChar;
+      millisReadTimeout = millis() + READ_TIMEOUT;
+      }
+    }
+    else {
+      yield();
+    }
   }
 
-  Serial.print(red);
-  Serial.print(", ");
-  Serial.print(green);
-  Serial.print(", ");
-  Serial.println(blue);
-  
-  digitalWrite(RGB_RED_PIN, red);
-  digitalWrite(RGB_GREEN_PIN, green);
-  digitalWrite(RGB_BLUE_PIN, blue);  
+  return -1; // code -1 = timed out waiting for new character
 }
