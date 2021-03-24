bfb0919987e5ed76e3f892a6bbe6c78a0f3348f0
Calvin Abel <n01300070@humbermail.ca>
2021-03-23 21:45:43 -0400

    Update ThermostatProject.ino
    Fixed empty message handling in readIncomingMessage() function again(x2)

--- Thermostat/ThermostatProject/ThermostatProject.ino
+++ Thermostat/ThermostatProject/ThermostatProject.ino
@@ -1,10 +1,10 @@
 /*
- * Author: Jay Patel & Rohan Mir
- * 
- * Target Device: Thermostat (HVAC Interface)
- * Target Board: ESP8266
- */
- 
+   Author: Jay Patel & Rohan Mir
+
+   Target Device: Thermostat (HVAC Interface)
+   Target Board: ESP8266
+*/
+
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include "firebaseCredentials.h" //Contains FIREBASE_HOST, FIREBASE_API_KEY, FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY
@@ -44,52 +44,52 @@ float humidity = 0.0;
 
 
 //GPIO0 Pin is to be unplugged after uploading
-void setup() 
+void setup()
 {
-   // Turn on Serial so we can verify expected colors via Serial Monitor
-    Serial.begin(115200);
-    delay(1000);
-    
-    // Connect to Wi-Fi
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    
-    // Display connection status
-    Serial.print("\nConnecting to Wi-Fi");
-    
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(250); //Wait until connection successful
-        Serial.print(".");
-    }
-    
-    Serial.print("\nSuccessfully connected to Wi-FI\n\n");
-    
-    // Set Firebase credentials
-    fbConfig.host = FIREBASE_HOST;
-    fbConfig.api_key = FIREBASE_API_KEY;
+  // Turn on Serial so we can verify expected colors via Serial Monitor
+  Serial.begin(115200);
+  delay(1000);
 
-    fbConfig.service_account.data.client_email = FIREBASE_CLIENT_EMAIL;
-    fbConfig.service_account.data.project_id = FIREBASE_PROJECT_ID;
-    fbConfig.service_account.data.private_key = FIREBASE_PRIVATE_KEY;
+  // Connect to Wi-Fi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-    fbAuth.token.uid = "Thermostat01"; //Manually assigned for now
+  // Display connection status
+  Serial.print("\nConnecting to Wi-Fi");
 
-    fbAuth.token.claims.add("device", true);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(250); //Wait until connection successful
+    Serial.print(".");
+  }
 
-    // Setup Firebase connection
-    Firebase.reconnectWiFi(true);
-    fbData.setResponseSize(4096);
-    Firebase.begin(&fbConfig, &fbAuth);
+  Serial.print("\nSuccessfully connected to Wi-FI\n\n");
 
-    fbPath = PATH_START + fbAuth.token.uid.c_str() + PATH_END;
+  // Set Firebase credentials
+  fbConfig.host = FIREBASE_HOST;
+  fbConfig.api_key = FIREBASE_API_KEY;
+
+  fbConfig.service_account.data.client_email = FIREBASE_CLIENT_EMAIL;
+  fbConfig.service_account.data.project_id = FIREBASE_PROJECT_ID;
+  fbConfig.service_account.data.private_key = FIREBASE_PRIVATE_KEY;
+
+  fbAuth.token.uid = "Thermostat01"; //Manually assigned for now
+
+  fbAuth.token.claims.add("device", true);
+
+  // Setup Firebase connection
+  Firebase.reconnectWiFi(true);
+  fbData.setResponseSize(4096);
+  Firebase.begin(&fbConfig, &fbAuth);
+
+  fbPath = PATH_START + fbAuth.token.uid.c_str() + PATH_END;
 }
 
 
 
 
-void loop() 
+void loop()
 {
-  if (Serial.available()) 
+  if (Serial.available())
   {
     messageResult = readIncomingMessage();
 
@@ -111,13 +111,13 @@ void loop()
       updateTempHumid();
     }
   }
-   
+
 }
 
 
 
 /*String getValue(String data, char separator, int index)
-{
+  {
     int found = 0;
     int strIndex[] = { 0, -1 };
     int maxIndex = data.length() - 1;
@@ -130,12 +130,12 @@ void loop()
         }
     }
     return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
-}*/
+  }*/
 
 
 
 
-int readIncomingMessage(void) 
+int readIncomingMessage(void)
 {
   unsigned long millisReadTimeout = millis() + READ_TIMEOUT;
   char currChar = 0;
@@ -145,70 +145,72 @@ int readIncomingMessage(void)
 
   message = "";
 
-  while (millis() < millisReadTimeout) 
+  while (millis() < millisReadTimeout)
   {
-    if (Serial.available() > 0) 
+    if (Serial.available() > 0)
     {
       currChar = Serial.read();
 
-      if (currChar == '\n') 
+      if (currChar == '\n')
       {
-        if (notData) 
+        if (notData)
         {
           return 0; // code 0 = finished reading log message
         } else if (message.length() <= 0) {
           return -2; // code -2 = resulting message is empty
         }
-        
+
         humidity = intString.toFloat();
         Serial.print("Set humidity variable to ");
         Serial.println(humidity);
         return 1; // code 1 = finished reading and interpreting data message
       }
-      
-      else if (!notData) 
+      else if (currChar == '\r') {
+        // Do nothing
+      }
+      else if (!notData)
       {
-        if (isDigit(currChar)) 
+        if (isDigit(currChar))
         {
           intString += currChar;
         }
-        else if (currChar == ',') 
+        else if (currChar == ',')
         {
           numCommas++;
-          
-          if(numCommas == 1) 
+
+          if (numCommas == 1)
           {
             temperature = intString.toFloat();
             Serial.print("Set temperature variable to ");
-            Serial.println(temperature);      
-          } 
-          
-          else 
+            Serial.println(temperature);
+          }
+
+          else
           {
             Serial.println("message is not data: too many commas");
             notData = true;
           }
-          
+
           intString = "";
         }
-        
-        else 
+
+        else
         {
-          //Serial.print("message is not data: char '");
-          //Serial.print(currChar);
-          //Serial.print("' is not used in data\n");
+          Serial.print("message is not data: char '");
+          Serial.print(currChar);
+          Serial.print("' is not used in data\n");
           notData = true;
         }
       }
 
       if (currChar != '\r')
       {
-      message += currChar;
-      millisReadTimeout = millis() + READ_TIMEOUT;
+        message += currChar;
+        millisReadTimeout = millis() + READ_TIMEOUT;
       }
     }
-    
-    else 
+
+    else
     {
       yield();
     }
@@ -222,57 +224,57 @@ int readIncomingMessage(void)
 
 
 
-void updateTempHumid() 
+void updateTempHumid()
 {
-    String path;
-    String message;
+  String path;
+  String message;
 
-  if(Serial.available()) 
+  if (Serial.available())
   {
     message = readIncomingMessage();
-    
-    
+
+
     path = fbPath + PATH_TEMPERATURE;
-    if (Firebase.RTDB.set(&fbData, path.c_str(), temperature)) 
+    if (Firebase.RTDB.set(&fbData, path.c_str(), temperature))
     {
-        Serial.printf("\nSuccessfully updated %s to %.1f\n", path.c_str(), temperature);
-    } 
-    else 
+      Serial.printf("\nSuccessfully updated %s to %.1f\n", path.c_str(), temperature);
+    }
+    else
     {
-        Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
+      Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
     }
 
-   path = fbPath + PATH_HUMIDITY;
+    path = fbPath + PATH_HUMIDITY;
     if (Firebase.RTDB.set(&fbData, path.c_str(), humidity))
     {
-        Serial.printf("\nSuccessfully updated %s to %.1f\n", path.c_str(), humidity);
-    } 
-    else 
+      Serial.printf("\nSuccessfully updated %s to %.1f\n", path.c_str(), humidity);
+    }
+    else
     {
-        Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
+      Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
     }
   }
-    updateTimeStamp();
+  updateTimeStamp();
 }
 
 
 
 
 
-void updateTimeStamp(void) 
+void updateTimeStamp(void)
 {
-    String path;
-    path = fbPath + PATH_TIMESTAMP;
-    if (Firebase.RTDB.setTimestamp(&fbData, path.c_str())) 
-    {
-        Serial.printf("Successfully updated %s to current time\n", path.c_str());
-    }
-    else 
-    {
-        Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
-    }
+  String path;
+  path = fbPath + PATH_TIMESTAMP;
+  if (Firebase.RTDB.setTimestamp(&fbData, path.c_str()))
+  {
+    Serial.printf("Successfully updated %s to current time\n", path.c_str());
+  }
+  else
+  {
+    Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
+  }
 
-    Serial.print("\n");
+  Serial.print("\n");
 }
 
 
@@ -280,48 +282,48 @@ void updateTimeStamp(void)
 
 
 
-/*void getHVAC() 
-{
+/*void getHVAC()
+  {
     String path;
     bool compressor;
     bool reverse;
     bool fan;
-    
+
     path = fbPath + PATH_COMPRESSOR;
-    if (Firebase.RTDB.getBool(&fbData, path.c_str())) 
+    if (Firebase.RTDB.getBool(&fbData, path.c_str()))
     {
       Serial.printf("Successfully read %s\n", path.c_str());
       compressor = fbData.boolData();
-    } 
-    else 
+    }
+    else
     {
       Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
     }
-   
-  
+
+
     path = fbPath + PATH_REVERSE;
-    if (Firebase.RTDB.getBool(&fbData, path.c_str())) 
+    if (Firebase.RTDB.getBool(&fbData, path.c_str()))
     {
       Serial.printf("Successfully read %s\n", path.c_str());
       reverse = fbData.boolData();
-    } 
-    else 
+    }
+    else
     {
       Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
     }
-  
-  
+
+
     path = fbPath + PATH_FAN;
-    if (Firebase.RTDB.getBool(&fbData, path.c_str())) 
+    if (Firebase.RTDB.getBool(&fbData, path.c_str()))
     {
       Serial.printf("Successfully read %s\n", path.c_str());
       fan = fbData.boolData();
-    } 
-    else 
+    }
+    else
     {
       Serial.printf("Failed to update %s: %s\n", path.c_str(), fbData.errorReason().c_str());
     }
 
     Serial.printf("%d, %d, %d\n",compressor, reverse, fan);
-    delay(5000);  
-}*/
+    delay(5000);
+  }*/
