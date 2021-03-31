df1026ab168cc6762cb25b3e331af0e6c6023aeb
jp27-eng <71619423+jp27-eng@users.noreply.github.com>
2021-03-30 22:21:27 -0400

    Update ThermostatProject_LED_TempHumid.ino

--- Thermostat/ThermostatProject_LED_TempHumid/ThermostatProject_LED_TempHumid.ino
+++ Thermostat/ThermostatProject_LED_TempHumid/ThermostatProject_LED_TempHumid.ino
@@ -13,110 +13,218 @@
 //PB14 = Yellow LED (Reverse Value)
 //PB15 = Green LED (Cooling Fan)
 
+#define READ_TIMEOUT 250
 
 const boolean IS_COMMON_ANODE = false; 
 DHT dht(PA1, DHT11);
 
+int messageResult = 0;
+String message = "";
+
+float tempRange = 0;
+float targetTemp = 0;
+
+bool compressor = false;  //Red LED
+bool reverse = false;     //Yellow LED
+bool fan = false;         //Green LED
+
+
 void setup() 
 {
-   // Set the RGB pins to output
+  // Set the LED pins to output
   pinMode(PB13, OUTPUT);
   pinMode(PB14, OUTPUT);
   pinMode(PB15, OUTPUT);
+  
   dht.begin();
+
+  Serial.begin(9600);
+  Serial2.begin(115200);  
 }
+
+
+
+void getTempAndHumid()
+{
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  Serial2.print(t);
+  Serial2.print(",");
+  Serial2.print(h);
+  Serial2.print("\n");
+ // String sendTo = String(t) + "\r\n"
+  Serial2.print("32.5/19.7\r\n");
+}
+
+
+
+
+void setHVAC()
+{
+  Serial.print(compressor);
+  Serial.print(", ");
+  Serial.print(reverse);
+  Serial.print(", ");
+  Serial.println(fan);
+  
+  digitalWrite(PB13, compressor);
+  digitalWrite(PB14, reverse);
+  digitalWrite(PB15, fan);  
+}
+
 
 
 
 void loop() 
 {
-
-  if (Serial.available() > 0) 
+  if (Serial2.available()) 
   {
-      char state = Serial.read();
+    messageResult = readIncomingMessage();
 
-      if (state == 'T' || state == 't')
+    if (messageResult == -1) {
+      Serial.println("Error reading message: timed out waiting for new character");
+    } else if (messageResult < -1) {
+      Serial.print("Error reading message: an unkown error occured with code ");
+      Serial.println(messageResult);
+    }
+
+    Serial.print("ESP8266: ");
+    Serial.println(message);
+
+    if (messageResult == 1) 
+    {
+      Serial.println("HVAC Interfaces");
+      setHVAC();
+    }
+  }
+}
+
+
+
+
+
+int readIncomingMessage(void) 
+{
+  unsigned long millisReadTimeout = millis() + READ_TIMEOUT;
+  char currChar = 0;
+  String intString = "";
+  int numCommas = 0;
+  boolean notData = false;
+
+  message = "";
+
+  while (millis() < millisReadTimeout) 
+  {
+    if (Serial.available() > 0) 
+    {
+      currChar = Serial.read();
+
+      if (currChar == '\n') 
       {
-        displayTemp();
-        delay(1000);  
-      }
-      if (state == 'H' || state == 'h')
-      {
-         Serial.print("LED IS ON ");
-         HVAC_State(HIGH, HIGH, HIGH);
-         delay(1000);
+        if (notData) 
+        {
+          return 0; // code 0 = finished reading log message
+        }
+        
+        tempRange = intString.toFloat();
+        //Serial.print("Set blue brightness variable to ");
+        //Serial.println(blueBright);
+        return 1; // code 1 = finished reading and interpreting data message
       }
       
-      if (state == 'R' || state == 'r')
+      else if (!notData) 
       {
-      // red
-        Serial.print("Red: ");
-        HVAC_State(HIGH, LOW, LOW);
-        delay(1000);
+        if (isDigit(currChar)) 
+        {
+          intString += currChar;
+        }
+        else if (currChar == ',') 
+        {
+          numCommas++;
+          
+          if (numCommas == 1) 
+          {
+            if(currChar == '0')
+            {
+              compressor = false;
+            }
+            else if(currChar == '1')
+            {
+              compressor = true;
+            }
+            else 
+            {
+              notData = true;
+            }
+          } 
+
+          else if (numCommas = 2)
+          {
+            if(currChar == '0')
+            {
+              reverse = false;
+            }
+            else if(currChar == '1')
+            {
+              reverse = true;
+            }
+            else 
+            {
+              notData = true;
+            }
+          }
+
+          else if (numCommas = 3)
+          {
+            if(currChar == '0')
+            {
+               fan = false;
+            }
+            else if(currChar == '1')
+            {
+               fan = true;
+            }
+            else 
+            {
+              notData = true;
+            }
+          }
+          
+          else if (numCommas = 4)
+          {
+            targetTemp =  intString.toFloat();
+          }
+          
+          else 
+          {
+            Serial.println("message is not data: too many commas");
+            notData = true;
+          }
+          
+          intString = "";
+        }
+        
+        else 
+        {
+          //Serial.print("message is not data: char '");
+          //Serial.print(currChar);
+          //Serial.print("' is not used in data\n");
+          notData = true;
+        }
       }
 
-      if (state == 'Y' || state == 'y')
+      if (currChar != '\r')
       {
-       // yellow
-        Serial.print("Yellow: ");
-        HVAC_State(LOW, HIGH, LOW);
-        delay(1000);
-      }
-
-      
-      if (state == 'G' || state == 'g')
-      {
-        // green
-        Serial.print("Green: ");
-         HVAC_State(LOW, LOW, HIGH);
-        delay(1000);
-      }
-    
-      if (state == 'L' || state == 'l')
-      {
-         Serial.print("LED IS OFF ");
-         HVAC_State(LOW, LOW, LOW);
+      message += currChar;
+      millisReadTimeout = millis() + READ_TIMEOUT;
       }
     }
-}
-
-
-/**
- * The serial reads Red('R', 'r'), Yellow('Y', 'y'), Green('G', 'g') and on/off switches which are High('H' 'h') and Low('L', 'l').
- * This function is based on https://gist.github.com/jamesotron/766994
- */
-void  HVAC_State(int red, int green, int yellow)
-{
-  // If a common anode LED, invert values
-  if(IS_COMMON_ANODE == true)
-  {
-    red = !red;
-    green = !green;
-    yellow = !yellow;
+    
+    else 
+    {
+      yield();
+    }
   }
 
-  Serial.print(red);
-  Serial.print(", ");
-  Serial.print(green);
-  Serial.print(", ");
-  Serial.println(yellow);
-  
-  digitalWrite(PB13, red);
-  digitalWrite(PB14, yellow);
-  digitalWrite(PB15, green);  
-}
-
-void displayTemp()
-{
-  float h = dht.readHumidity();
-  float tc = dht.readTemperature();
-  float tf = dht.convertCtoF(tc);
-  
-  Serial.print("\nTemperature (Celsius): ");
-  Serial.print(tc);
-  Serial.print("\nTemperature (Fahrenheit): ");
-  Serial.print(tf);
-  Serial.print("\nHumidity: ");
-  Serial.print(h);
-  Serial.print("\n");
+  return -1; // code -1 = timed out waiting for new character
 }
